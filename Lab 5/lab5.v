e4ede3ea944c137221326fbd893e4a5f9ac35ca0
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-10-18 17:39:42 +0800

    Update lab5.v

--- Lab 5/lab5.v
+++ Lab 5/lab5.v
@@ -19,58 +19,53 @@ output reg clked;
 reg [23:0]decnt;
 parameter bound=24'hffffff;
 always@(posedge clk or negedge rst)begin
-	if(~rst)begin
-		decnt<=0;
-		clked<=0;
-	end
-	else begin
-		if(~in)begin
-			if(decnt<bound)begin
-				decnt<=decnt+1;
-				clked<=1;
-			end
-			else begin
-				decnt<=decnt;
-				clked<=1;
-			end
-		end
-		else begin
-			decnt<=0;
-			clked<=0;
-		end
-	end
+   if(~rst)begin
+      decnt<=0;
+      clked<=0;
+   end
+   else begin
+      if(~in)begin
+         if(decnt<bound)begin
+	    decnt<=decnt+1;
+	    clked<=1;
+         end
+         else begin
+	    decnt<=decnt;
+	    clked<=1;
+	 end
+      end
+      else begin
+         decnt<=0;
+	 clked<=0;
+      end
+   end
 end
 endmodule
 //------------------------------------------
 module div(divclkout,clk,rst,speedup,speedD);
 output divclkout;
-   input clk;
-	input rst;
-	input speedup;
-	input speedD;
-
+input clk;
+input rst;
+input speedup;
+input speedD;
 reg [25:0] divclk;
 reg [4:0] speed;
 assign divclkout = divclk[speed];
-
-always@(posedge clk or negedge rst)
-begin
+always@(posedge clk or negedge rst)begin
    if(~rst)
-	   divclk <= 26'd0;
-	else begin
-	   divclk <= divclk + 1'd1;
-	end
+      divclk <= 26'd0;
+   else begin
+      divclk <= divclk + 1'd1;
+   end
 end
-
-always@(posedge clk or negedge rst)
-begin
+always@(posedge clk or negedge rst)begin
    if(~rst)
-	   speed = 5'd23;
-	else if(speedup)
-	   speed = 5'd21;
-	else if(speedD)
-	   speed = 5'd25;
-	else
+      speed = 5'd23;
+   else if(speedup)
+      speed = 5'd21;
+   else if(speedD)
+      speed = 5'd25;
+   else
       speed = 5'd23;	
 end
 endmodule
@@ -81,65 +76,63 @@ output reg [2:0]ledout;
 reg [1:0]state;
 input clk,rst;
 parameter init=2'd0,red=2'd1,green=2'd2,yellow=2'd3;
-always@(posedge clk or negedge rst)
-begin
-   if(~rst)
-	   begin
-		   sec<=4'd0;
-			state<=init;
-		end
-	else begin
-	   case(state)
-		   init:begin
-			   sec<=4'd0;
-				state<=red;
-			end
-			red:begin
-			   if(sec<4'd9)
-				   sec<=sec+1;
-				else begin
-				   sec<=4'd0;
-					state<=green;
-				end
-			end
-			green:begin
-			   if(sec<4'd6)
-				   sec<=sec+1;
-				else begin
-				   sec<=4'd0;
-					state<=yellow;
-				end
-			end
-			yellow:begin
-			   if(sec<4'd3)
-				   sec<=sec+1;
-				else begin
-				   sec<=4'd0;
-					state<=red;
-				end
-			end
-			default:begin
-			   sec<=4'd0;
-				state<=init;
-		   end
-		endcase
+always@(posedge clk or negedge rst)begin
+   if(~rst)begin
+      sec<=4'd0;
+      state<=init;
+   end
+   else begin
+      case(state)
+        init:begin
+		sec<=4'd0;
+		state<=red;
 	end
+	red:begin
+		if(sec<4'd9)
+		   sec<=sec+1;
+		else begin
+		   sec<=4'd0;
+		   state<=green;
+		end
+	end
+	green:begin
+	    	if(sec<4'd6)
+		   sec<=sec+1;
+		else begin
+		   sec<=4'd0;
+		   state<=yellow;
+		end
+	end
+	yellow:begin
+		if(sec<4'd3)
+		   sec<=sec+1;
+		else begin
+		   sec<=4'd0;
+		   state<=red;
+		end
+	end
+	default:begin
+	 	sec<=4'd0;
+		state<=init;
+	end
+      endcase
+   end
 end
 always@(state)begin
    case(state)
-	   init:ledout<=3'b000;
-		red:ledout<=3'b100;
-		green:ledout<=3'b010;
-		yellow:ledout<=3'b001;
-		default:ledout<=2'b000;
-	endcase
+      init:ledout<=3'b000;
+      red:ledout<=3'b100;
+      green:ledout<=3'b010;
+      yellow:ledout<=3'b001;
+      default:ledout<=2'b000;
+   endcase
 end
 endmodule
 //-----------------------------------------------
 module seg7(out,outselect,in);
-   input [3:0] in;
-	output [3:0] outselect;
-	output reg [7:0] out;
+input [3:0] in;
+output [3:0] outselect;
+output reg [7:0] out;
 assign outselect=4'b0000;
 always@(*)begin
    case(in)
@@ -160,7 +153,7 @@ always@(*)begin
 	4'd13: begin out=8'b10000101; end
 	4'd14: begin out=8'b01100001; end
 	4'd15: begin out=8'b01110001; end
-   default:;
-	endcase
+   	default:;
+   endcase
 end
 endmodule
