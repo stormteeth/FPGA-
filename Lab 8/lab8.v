afd79ee3e85e63b330d271b420f848228bd07cd0
stormteeth <55481909+stormteeth@users.noreply.github.com>
2021-10-31 15:03:29 +0800

    Update lab8.v

--- Lab 8/lab8.v
+++ Lab 8/lab8.v
@@ -22,116 +22,114 @@ assign romdata[7]=8'h32;
 reg[19:0]divclk;
 always@(posedge clk or negedge rst)begin
    if(~rst)
-	   divclk<=20'd0;
-	else
-	   divclk<=divclk+1;
+      divclk<=20'd0;
+   else
+      divclk<=divclk+1;
 end
-
 always@(posedge divclk[19] or negedge rst)begin
    if(~rst)begin
-	  state<=idle;
-	  datacnt<=4'd0;
+      state<=idle;
+      datacnt<=4'd0;
+   end
+   else begin
+      case(state)
+         idle:begin
+            datacnt<=4'd0;
+	    state<=setup_mode;
+         end
+         //----Mode Command--------
+         setup_mode:state<=write_mode;
+         write_mode:state<=setup_display;
+         //----Display Command-----
+         setup_display:state<=write_display;
+         write_display:state<=setup_clear;
+         //----Clear Command-------
+         setup_clear:state<=write_clear;
+         write_clear:state<=setup_position;
+         //----Position Command----
+         setup_position:state<=write_position;
+         write_position:state<=setup_data;
+         //----Data----------------
+         setup_data:state<=write_data;
+         write_data:begin
+         if(datacnt<4'd7)begin
+	    datacnt<=datacnt+1;
+	    state<=setup_data;
 	 end
-	 else begin
-	    case(state)
-		    idle:begin
-			    datacnt<=4'd0;
-				 state<=setup_mode;
-			 end
-			 //----Mode Command--------
-			 setup_mode:state<=write_mode;
-			 write_mode:state<=setup_display;
-			 //----Display Command-----
-			 setup_display:state<=write_display;
-			 write_display:state<=setup_clear;
-		    //----Clear Command-------
-			 setup_clear:state<=write_clear;
-			 write_clear:state<=setup_position;
-			 //----Position Command----
- 			 setup_position:state<=write_position;
-			 write_position:state<=setup_data;
-          //----Data----------------
-			 setup_data:state<=write_data;
-			 write_data:begin
-			    if(datacnt<4'd7)begin
-				    datacnt<=datacnt+1;
-					 state<=setup_data;
-				 end
-				 else
-				    state<=state;
-			 end
-			 default:state<=idle;
-		 endcase
-	end
+	 else
+	    state<=state;
+         end
+         default:state<=idle;
+      endcase
+   end
 end
 //-----------------------------------------
 always@(state)begin
    case(state)
-	   idle:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'd0;
-		end
-	//--------mode command--------
-		setup_mode:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0011_1000;
-		end
-		write_mode:begin
-		   lcd_en<=1'b0;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0011_1000;
-		end
-	//------display command-------
-		setup_display:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0000_1110;
-		end
-		write_display:begin
-		   lcd_en<=1'b0;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0000_1110;
-		end
-	//---------clear command------
-		setup_clear:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0000_0001;
-		end
-		write_clear:begin
-		   lcd_en<=1'b0;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0000_0001;
-		end
-   //------position command------		
-		setup_position:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b1000_0000;
-		end	
-		write_position:begin
-		   lcd_en<=1'b0;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b1000_0000;
-		end	
-   //------data------------------  		
-		setup_data:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b1;
-			lcd_db<=romdata[datacnt];
-		end
-		write_data:begin
-		   lcd_en<=1'b0;
-			lcd_rs<=1'b1;
-			lcd_db<=romdata[datacnt];
-		end
-		
-		default:begin
-		   lcd_en<=1'b1;
-			lcd_rs<=1'b0;
-			lcd_db<=8'b0;	
+      idle:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'd0;
+      end
+      //--------mode command--------
+      setup_mode:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0011_1000;
+      end
+      write_mode:begin
+         lcd_en<=1'b0;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0011_1000;
+      end
+      //------display command-------
+      setup_display:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0000_1110;
+      end
+      write_display:begin
+         lcd_en<=1'b0;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0000_1110;
+      end
+      //---------clear command------
+      setup_clear:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0000_0001;
+      end
+      write_clear:begin
+      lcd_en<=1'b0;
+      lcd_rs<=1'b0;
+      lcd_db<=8'b0000_0001;
+      end
+      //------position command------		
+      setup_position:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b1000_0000;
+      end	
+      write_position:begin
+         lcd_en<=1'b0;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b1000_0000;
+      end	
+      //------data------------------  		
+      setup_data:begin
+      lcd_en<=1'b1;
+      lcd_rs<=1'b1;
+      lcd_db<=romdata[datacnt];
+      end
+      write_data:begin
+         lcd_en<=1'b0;
+	 lcd_rs<=1'b1;
+	 lcd_db<=romdata[datacnt];
+      end
+      default:begin
+         lcd_en<=1'b1;
+	 lcd_rs<=1'b0;
+	 lcd_db<=8'b0;	
       end
    endcase
 end	
